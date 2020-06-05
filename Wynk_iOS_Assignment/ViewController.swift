011c2df2dd79b0bc756ede96d67fb4da3ee3289d
Raj Maurya <raj.maurya@xebia.com>
2020-06-05 19:23:38 +0530

    Initial Commit

---
+++ Wynk_iOS_Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wynk_iOS_Assignment
+//
+//  Created by Raj on 05/06/20.
+//  Copyright © 2020 Xebia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
